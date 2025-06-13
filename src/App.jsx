c29953a058f00f850caa82fb3a471a41d5b315bb
devabdullah000 <devabdullah000@gmail.com>
2025-06-13 20:28:55 +0500

    corrected commit message

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
     <>
     <h1>PRACTICE DEVOPS</h1>
     <h2>Welcome to devops. learning setting upstream with remote repo</h2>
+    <p>This is a practice project to learn about DevOps concepts and workflows.</p>
     </>
   )
 }
